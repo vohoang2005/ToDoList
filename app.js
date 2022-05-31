6a474c8ccc5611fcdbe6519b0af923df668484a9
vohoang2005 <vohoang2005@gmail.com>
2022-05-31 16:24:35 -0700

    updated list.ejs and app.js

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { userInfo } = require("os");
 const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req, res) {
     var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };    
 
-    if(today.getDay() === 6 || today.getDay() === 0) {
-        res.send("Yay it's the weekend!")
-    }else{
-        res.send("Boo! I have to work!")
-    }
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post("/", function(req, res) {
+    var item = req.body.newitem;
+    items.push(item);
+    
+    res.redirect("/");
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
